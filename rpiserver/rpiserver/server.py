64b852c2121de87cca6e94219e70ed3b5fc3ef8e
Hannah Willoughby <hannah@hannahwilloughby.dev>
2023-11-14 16:26:52 -0500

    Beef out home page

--- rpiserver/rpiserver/server.py
+++ rpiserver/rpiserver/server.py
@@ -2,6 +2,7 @@ from starlette.responses import HTMLResponse
 from fastapi import FastAPI
 from pydantic import BaseModel
 import RPi.GPIO as GPIO
+from datetime import datetime
 
 
 GPIO.setmode(GPIO.BCM)
@@ -11,6 +12,7 @@ GPIO.setwarnings(False)
 class Valve(BaseModel):
     gpio: int
     is_open: bool = False
+    last_changed: datetime = datetime.now()
     
     def init(self):
         print(f"Initializing valve {self.gpio}")
@@ -21,11 +23,13 @@ class Valve(BaseModel):
         print(f"Opening valve {self.gpio}")
         GPIO.output(self.gpio, GPIO.HIGH)
         self.is_open = True
+        self.last_changed = datetime.now()
         
     def close(self):
         print(f"Closing valve {self.gpio}")
         GPIO.output(self.gpio, GPIO.LOW)
         self.is_open = False
+        self.last_changed = datetime.now()
 
 
 class LED(BaseModel):
@@ -88,19 +92,27 @@ def root():
 
 def generate_html_root_response() -> HTMLResponse:
   state = "opened" if valve.is_open else "closed"
+  state_color = "blue" if valve.is_open else "green"
+  time_in_state = datetime.now() - valve.last_changed
   html_content = f"""
-  <html>
+  <html style="background-color: dodgerblue; color: white;">
     <head>
       <title>Poppyland Raincloud</title>
     </head>
     <body>
-      <div>
-        <h2>Raincloud Status<h2>
-        <p>{state}</p>
+      <div id="header">
+        <h1>Poppyland Raincloud</h1>
       </div>
-      <div>
-        <button onclick="location.href='/api/valves/open'" type="button">OPEN</button>
-        <button onclick="location.href='/api/valves/close'" type="button">CLOSE</button>
+      <div id="tile-container" style="display: flex; flex-direction: row; gap: 50px;">
+        <div id="status" style="background-color: white; color: black; border: 1px solid lightgrey; padding: 20px;">
+          <h2>Raincloud Status<h2>
+          <p style="color: {state_color}">{state}</p>
+          <p>Time in this state: {time_in_state}</p>
+        </div>
+        <div id="controls" style="background-color: white; color: black;">
+          <button onclick="location.href='/api/valves/open'" type="button" style="padding: 50px; background-color: blue; color: white; border-radius: 50px;">OPEN</button>
+          <button onclick="location.href='/api/valves/close'" type="button" style="padding: 50px; background-color: green; color: white; border-radius: 50px;">CLOSE</button>
+        </div>
       </div>
     </body>
   </html>
